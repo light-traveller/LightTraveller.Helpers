c9e5f97ce56ee3644201bb97f7a77f726440e62a
light-traveller <light-traveller@outlook.com>
2023-02-01 21:03:22 +0400

    - Change method name (GetNumberOfPages -> GetPageCount)
    - Add int overload

--- src/LightTraveller.Helpers.UnitTests/PagingExtensionTests.cs
+++ src/LightTraveller.Helpers.UnitTests/PagingExtensionTests.cs
@@ -6,34 +6,34 @@ public class PagingExtensionTests
     public void WithZeroPageSize_GetNumberOfPages_Should_Throw()
     {
         var totalRecords = 100L;
-        _ = Assert.Throws<ArgumentException>(() => totalRecords.GetNumberOfPages(pageSize: 0));
+        _ = Assert.Throws<ArgumentException>(() => totalRecords.GetPageCount(pageSize: 0));
     }
 
     [Fact]
     public void WithNegativePageSize_GetNumberOfPages_Should_Throw()
     {
         var totalRecords = 100L;
-        _ = Assert.Throws<ArgumentException>(() => totalRecords.GetNumberOfPages(pageSize: -5));
+        _ = Assert.Throws<ArgumentException>(() => totalRecords.GetPageCount(pageSize: -5));
     }
 
     [Fact]
     public void WithZeroTotalRecord_GetNumberOfPages_Should_ReturnZero()
     {
         var totalRecords = 0L;
-        Assert.Equal(0, totalRecords.GetNumberOfPages(pageSize: 5));
+        Assert.Equal(0, totalRecords.GetPageCount(pageSize: 5));
     }
 
     [Fact]
     public void WithZeroRamainder_GetNumberOfPages_Should_ReturnTotalRecordsDividedByPageSize()
     {
         var totalRecords = 100L;
-        Assert.Equal(10, totalRecords.GetNumberOfPages(pageSize: 10));
+        Assert.Equal(10, totalRecords.GetPageCount(pageSize: 10));
     }
 
     [Fact]
     public void WithNonZeroRamainder_GetNumberOfPages_Should_ReturnTotalRecordsDividedByPageSizePlusOne()
     {
         var totalRecords = 101L;
-        Assert.Equal(11, totalRecords.GetNumberOfPages(pageSize: 10));
+        Assert.Equal(11, totalRecords.GetPageCount(pageSize: 10));
     }
 }
--- src/LightTraveller.Helpers/PagingExtensions.cs
+++ src/LightTraveller.Helpers/PagingExtensions.cs
@@ -4,7 +4,18 @@ namespace LightTraveller.Helpers;
 
 public static class PagingExtensions
 {
-    public static long GetNumberOfPages(this long totalRecords, int pageSize)
+    public static long GetPageCount(this long totalRecords, int pageSize)
+    {
+        _ = Guard.ZeroOrNegative(pageSize);
+
+        if (totalRecords <= 0)
+            return 0;
+
+        var pages = totalRecords / pageSize;
+        return totalRecords % pageSize == 0 ? pages : pages + 1;
+    }
+
+    public static int GetPageCount(this int totalRecords, int pageSize)
     {
         _ = Guard.ZeroOrNegative(pageSize);
 
