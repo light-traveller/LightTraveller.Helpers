6620e86492c3a7922ce2b0b587f5dbf933bc97f2
light-traveller <kamran_saeedi@yahoo.com>
2023-02-03 17:17:10 +0400

    Add non-generic IsNullOrEmpty for IEnumerable.

--- src/LightTraveller.Helpers/EnumerableExtensions.cs
+++ src/LightTraveller.Helpers/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using LightTraveller.Guards;
+﻿using System.Collections;
+using LightTraveller.Guards;
 
 namespace LightTraveller.Helpers;
 
@@ -12,6 +13,31 @@ public static class EnumerableExtensions
     /// <param name="predicate">A function to test each element of the collection.</param>
     /// <returns><see cref="true"/> if the collection does not contain any elements that satisfy the condition; otherwise, <see cref="false"/>.</returns>
     public static bool None<T>(this IEnumerable<T> collection, Func<T, bool> predicate) => !collection.Any(predicate);
+
+    /// <summary>
+    /// Determines whether an <see cref="IEnumerable"/> is null or an empty collection.
+    /// </summary>    
+    /// <param name="collection">The <see cref="IEnumerable"/> to check.</param>
+    /// <returns><see cref="true"/> if the collection is null or has no elements; otherwise, <see cref="false"/>.</returns>
+    public static bool IsNullOrEmpty(this IEnumerable collection)
+    {
+        if (collection is null)
+            return true;
+
+        if (collection is ICollection col)
+            return col.Count == 0;
+
+        var e = collection.GetEnumerator();
+        return Using(e, Empty);
+
+        static bool Empty(IEnumerator enumerator)
+        {
+            if (!enumerator.MoveNext())
+                return true;
+
+            return false;
+        }
+    }
 
     /// <summary>
     /// Performs the specified action on each element of the collection.
@@ -43,4 +69,24 @@ public static class EnumerableExtensions
     /// <param name="collection">The <see cref="IEnumerable{T}"/> to check.</param>
     /// <returns><see cref="true"/> if the collection is null or has no elements; otherwise, <see cref="false"/>.</returns>
     public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection) => collection is null || !collection.Any();
+
+    /// <summary>
+    /// Executes a delegate using the passed <see cref="IEnumerator"/> argument and returns the result of that delegate.
+    /// </summary>
+    /// <typeparam name="TResult">Return type of the delegate.</typeparam>
+    /// <param name="enumerator">An <see cref="IEnumerator"/>.</param>
+    /// <param name="func">A <see cref="Func{T, TResult}"/>.</param>
+    /// <returns>The result of the delegate.</returns>
+    private static TResult Using<TResult>(IEnumerator enumerator, Func<IEnumerator, TResult> func)
+    {
+        try
+        {
+            return func(enumerator);
+        }
+        finally
+        {
+            if (enumerator is IDisposable d)
+                d.Dispose();
+        }
+    }
 }
