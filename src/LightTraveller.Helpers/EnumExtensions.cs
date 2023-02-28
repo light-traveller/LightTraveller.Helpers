702f5ddd5f075658d0ee67da0eb44abb56fe6233
light-traveller <light-traveller@outlook.com>
2023-02-28 21:50:43 +0400

    Add Enum.GetAttribute<TAttrib>& Enum.GetDisplayName

---
+++ src/LightTraveller.Helpers/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace LightTraveller.Helpers;
+
+public static class EnumExtensions
+{
+    public static TAttrib? GetAttribute<TAttrib>(this Enum e) where TAttrib : Attribute
+    {
+        return e.GetType().GetMember(e.ToString()).First().GetCustomAttribute<TAttrib>();
+    }
+
+    public static string? GetDisplayName(this Enum e)
+    {
+        return e.GetAttribute<DisplayAttribute>()?.Name;
+    }
+}
